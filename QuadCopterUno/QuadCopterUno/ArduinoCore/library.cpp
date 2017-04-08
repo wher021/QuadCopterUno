77cdc13ddde21a25290a5a923214e5215551345b
Willy Herrera <wille_021@hotmail.com>
2017-04-08 10:51:59 +0200

    Initial checkin

---
+++ QuadCopterUno/QuadCopterUno/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 2017-04-08 10:49:05
+ * Author : willy
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
